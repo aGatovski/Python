4f4a04ec18712f6464464a2e4ea2e28e2ce0273a
Gatovski <antonio.gatovski@sap.com>
2026-04-14 16:40:34 +0300

    Added category as optional parameter. Fixed amount validation

--- BudgetApp/transaction.py
+++ BudgetApp/transaction.py
@@ -2,7 +2,7 @@ import re
 
 class Transaction:
     TRANSACTION_CATEGORIES = [
-        "Grocieries",
+        "Groceries",
         "Gas",
         "Transport",
         "Entertainment",
@@ -11,20 +11,22 @@ class Transaction:
     ]
 
     DATE_PATTERN = re.compile(r"^20\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$")
-    AMOUNT_PATTERN = re.compile(r'[+-]\d+(\.\d+)?')
+    AMOUNT_PATTERN = re.compile(r'^-?\d+(\.\d+)?$')
 
-    def __init__(self,date: str,description: str,amount: str):
+    def __init__(self,date: str, description: str,amount: str,category=None):
         if not self.DATE_PATTERN.match(date):
             raise ValueError(f"Invalid date format: {date}. Expected YYYY-MM-DD.")
         self.date = date
 
         if not self.AMOUNT_PATTERN.match(amount):
-            raise ValueError(f"Invalid amount: {amount}. Expected +/-Amount.")
+            raise ValueError(f"Invalid amount: {amount}. Expected a positive number or a negative number prefixed with -.")
         self.amount = amount
 
         self.description = description
-        self.category = self.getCategory(self.description)
-
+        if category is not None:
+            self.category = self.getCategory(category)
+        else:
+            self.category = self.getCategory(self.description)
 
     def __str__(self):
         return (f"Transaction details: Date: {self.date} Category: {self.category} Description: {self.description} Amount: {self.amount}")
